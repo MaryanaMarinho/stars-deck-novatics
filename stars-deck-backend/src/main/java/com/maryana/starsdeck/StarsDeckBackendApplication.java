523bd0b58879bd8e76100b7818d85ed7a41da0c9
MaryanaMarinho <maryanalirius@hotmail.com>
2020-02-15 19:50:44 -0300

    create project

---
+++ stars-deck-backend/src/main/java/com/maryana/starsdeck/StarsDeckBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maryana.starsdeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarsDeckBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarsDeckBackendApplication.class, args);
+    }
+
+}
---
+++ stars-deck-backend/src/test/java/com/maryana/starsdeck/StarsDeckBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryana.starsdeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarsDeckBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
